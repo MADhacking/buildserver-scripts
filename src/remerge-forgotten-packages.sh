4522092191027a633c47c815289301c1ea8271ea
max <>
2010-02-08 03:16:26 +0000

    Try that emerge again shall we?

--- src/remerge-forgotten-packages.sh
+++ src/remerge-forgotten-packages.sh
@@ -10,11 +10,6 @@ if [ -z "$BUILDSPACE_NAME" ]; then
 fi
 
 # Remerge forgotten binaries!
-#emerge -pvek world --with-bdeps y 2>&1 1>&1 | awk '/\[ebuild/ { print "="$4 }' | xargs emerge -1
-#emerge -pveK world --with-bdeps y 2>&1 1>&1 | awk -F "] " '/\[ebuild.+\]/ { print $2 }' | xargs -r emerge -1pv
-#emerge -pveK world --with-bdeps y 2>&1 1>&1 | awk -F "] " '/\[ebuild.+\]/ { print $2 }' | xargs -r emerge -1 --keep-going
-
-emerge -pveK world --with-bdeps y 2>&1 1>&1 | \
-	awk -F "] " '/\[ebuild.+\]/ { print $2 }' | \
-		xargs -r -I % { emerge -1pv % ; emerge -1 --keep-going % }
-	
+emerge -pvek world --with-bdeps y 2>&1 1>&1 | \
+	awk -F "] " '/\[ebuild.+\]/ { print $2 }' | xargs | \
+		xargs -r -I {} emerge -1pv {} \; emerge -1 --keep-going {} \;
