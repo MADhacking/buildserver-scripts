8125d23578ed35cfd5c88820a230fb03e83c4e47
max <>
2010-03-11 17:14:58 +0000

    Add some output and patch fuzzyness.

--- src/auto-patch-portage.sh
+++ src/auto-patch-portage.sh
@@ -4,13 +4,15 @@ patchroot=/mnt/repositories/patches
 
 pushd $patchroot > /dev/null
 
-patches=`find -type f`
-for p in patches; do
-	pushd /mnt/repositories/testing/portage > /dev/null
-	files=`find -path "$p"`
-	for f in files; do
-		patch $f < $patchroot/$p
-	done
-done	
+patches=`find -type f | sort`
+for p in $patches; do
+        pushd /mnt/repositories/testing/portage > /dev/null
+        echo "Applying patch $p to :"
+        files=`find -path "$p" | sort`
+        for f in $files; do
+                echo "    $f"
+                patch -s -F 3 $f < $patchroot/$p
+        done
+done
 
 popd > /dev/null
