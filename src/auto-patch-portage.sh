bcf18539c8ae364fd05cbf3030bc96493a0b55a5
max <>
2010-03-11 17:54:51 +0000

    Tidy up directory pushing and popping. Rebuild manifest if an ebuild was updated. Ignore already applied patches.

--- src/auto-patch-portage.sh
+++ src/auto-patch-portage.sh
@@ -3,16 +3,21 @@
 patchroot=/mnt/repositories/patches
 
 pushd $patchroot > /dev/null
-
 patches=`find -type f | sort`
-for p in $patches; do
-        pushd /mnt/repositories/testing/portage > /dev/null
-        echo "Applying patch $p to :"
+popd > /dev/null
+
+pushd /mnt/repositories/testing/portage > /dev/null
+for p in "$patches"; do
+        echo "    Applying patch $p to :"
         files=`find -path "$p" | sort`
         for f in $files; do
-                echo "    $f"
-                patch -s -F 3 $f < $patchroot/$p
+                echo -n "        $f"
+                patch -s -N -F 3 $f < $patchroot/$p
+		if [ "${f##*.}" == "ebuild" ]; then
+			echo -n " - rebuilding manifest"
+			ebuild $f digest > /dev/null
+		fi
+		echo
         done
 done
-
 popd > /dev/null
