8da7101e9b4d7e34435e9b749c6caeb0762c05b0
max <>
2010-07-16 14:49:17 +0000

    Added help screen. Added error checking. Minor enhancements.

--- src/update-portage-metadata.sh
+++ src/update-portage-metadata.sh
@@ -1,22 +1,30 @@
 #! /bin/bash
 
-# Update environment and source the profile
-env-update &>/dev/null
+function helpscreen
+{
+	echo "Usage: update-portage-metadata"
+	echo "Rebuilds the portage metadata and, if installed, rebuilds the eix cache"
+}
+
+# If we have been run with parameters then display help and quit
+[[ -n ${1} ]] && helpscreen && exit
+
+# Source the profile
 source /etc/profile &>/dev/null
 
 # Check to make sure we are being run from inside the buildspace.
-if [ -z "$BUILDSPACE_NAME" ]; then
-  echo "ERROR: $0 should only be run from inside a buildspace!" >&2
-  exit
+if [[ -z "$BUILDSPACE_NAME" ]]; then
+	echo "ERROR: $0 should only be run from inside a buildspace!" >&2
+	exit
 fi
 
 echo -n "Rebuilding metadata cache for $BUILDSPACE_NAME build-space..." 
 emerge --metadata --quiet 1>/dev/null
-echo "...done."
+(( ! $? )) && echo "done." || echo "failed."
 
 # If eix is installed then update the cache
-if [ -x /usr/bin/eix-update ]; then
-  echo -n "Rebuilding eix cache for $BUILDSPACE_NAME build-space..."
-  eix-update --quiet
-  echo "...done."
+if [[ -x /usr/bin/eix-update ]]; then
+	echo -n "Rebuilding eix cache for $BUILDSPACE_NAME build-space..."
+	eix-update --quiet
+	(( ! $? )) && echo "done." || echo "failed."
 fi
